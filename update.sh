225e40e0d1ac4ccb493bbc315a1f141900246155
AGR Technology <30744294+agrtechnology@users.noreply.github.com>
2018-05-20 21:56:10 +1000

    Update update.sh
    Added -y flags to automatically run the command without asking to install each set of packages

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-sudo apt-get autoremove
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
